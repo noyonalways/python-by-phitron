b66919fa2cc38586e844754b057d4e8604830172
noyonalways <noyonrahman2003@gmail.com>
2024-02-07 21:07:42 +0600

    feat: lecture-2: string concatenation complete

---
+++ day-11/49.py
@@ -0,0 +1,11 @@
+# Lecture 2: String Concatenation
+
+string_1 = "Hello"
+string_2 = "xyz"
+new_string_1 = string_1+string_2
+new_string_2 = string_1+", " +string_2
+print(new_string_1)
+print(new_string_2)
+
+# 2^3 = 2*2*2 
+print("Hello world, "*3)
